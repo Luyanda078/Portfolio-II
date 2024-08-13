5992df56dbae565c31d8357a5bd0b549004a070b
Luyanda <luyandashamase5@gmail.com>
2024-08-13 15:44:25 +0200

    ACP

---
+++ Portfolio-II/src/components/Add.jsx
@@ -0,0 +1,19 @@
+function AddTransaction() {
+    return( 
+    <div>
+        <h1>
+            <h3>Add a new transaction</h3>
+            </h1>
+        <input type="text" placeholder="Enter item"/>
+        <br></br>
+        <input type="text" placeholder="Enter amount"/>
+        <br></br>
+        <select></select>
+        <br></br>
+        <button>Add transaction</button>
+
+
+    </div>
+    );
+    }
+    export default AddTransaction
---
+++ Portfolio-II/src/components/login.jsx
@@ -0,0 +1,13 @@
+// function Login() {
+// return( 
+// <div>
+//     <h1>Login Page</h1>
+
+//     <button>Go to Home Page</button>
+
+// </div>)
+   
+
+
+// }
+// export default Login
